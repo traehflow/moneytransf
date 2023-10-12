3cf8917065abf7d0b8c278b9add4d2e6c2673964
Vladimir Georgiev <wolfheart@abv.bg>
2023-10-12 14:16:09 +0300

    docker compose

--- moneytransfapp/src/test/java/com/vsoft/moneytransf/ProbaApplicationTests.java
+++ moneytransfapp/src/test/java/com/vsoft/moneytransf/ProbaApplicationTests.java
@@ -3,7 +3,7 @@ package com.vsoft.moneytransf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProbaApplicationTests {
 
 	@Test
